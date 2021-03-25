65a62519275b2797d6f3e146fcb9d3c9e053f4ca
kevin2003123 <kevinreyes005@gmail.com>
2021-03-25 15:18:55 -0300

    last

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express'); 
+const path = require('path');
 const app = express();
-app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-     res.sendFile('index.html', {root: __dirname});
+    res.sendFile(path.resolve(__dirname,'home.html'));
 });
+console.log("in");
 app.listen(process.env.PORT || 5000)
